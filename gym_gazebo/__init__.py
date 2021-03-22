a3b49fa14b86a48855228423bb5c63038792f497
andrewchyn <andrewyuliangchen@gmail.com>
2021-03-21 22:02:43 -0700

    removed extraneous files

--- gym_gazebo/__init__.py
+++ gym_gazebo/__init__.py
@@ -5,19 +5,6 @@ logger = logging.getLogger(__name__)
 
 # Gazebo
 # ----------------------------------------
-
-# cart pole
-register(
-    id='GazeboCartPole-v0',
-    entry_point='gym_gazebo.envs.gazebo_cartpole:GazeboCartPolev0Env',
-)
-
-register(
-	id='Gazebo_Lab06-v0',
-	entry_point='gym_gazebo.envs.gazebo_lab06:Gazebo_Lab06_Env',
-	max_episode_steps=3000,
-)
-
 register(
 	id='GazeboCompEnv-v0',
 	entry_point='gym_gazebo.envs.competition_env:GazeboCompEnv',
